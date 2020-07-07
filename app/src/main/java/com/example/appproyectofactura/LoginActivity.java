f89bda08ca537ad7e456e15a99764206194ca5e2
Gustavo Alegre <gustavoalegre1@hotmail.com>
2020-07-07 00:04:16 -0500

    20200706 Notificaciones Firebase

--- app/src/main/java/com/example/appproyectofactura/LoginActivity.java
+++ app/src/main/java/com/example/appproyectofactura/LoginActivity.java
@@ -1,11 +1,17 @@
 package com.example.appproyectofactura;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -15,6 +21,10 @@ import com.android.volley.Response.ErrorListener;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 import java.security.MessageDigest;
 import java.util.HashMap;
@@ -25,9 +35,13 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class LoginActivity extends AppCompatActivity {
 
+    String gcmRegId = "";
+
     EditText edtUsuario, edtPassword;
     Button btnLogin;
     String textoPassword;
+
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +59,42 @@ public class LoginActivity extends AppCompatActivity {
                 validarUsuario("https://proyectoinformatico03.000webhostapp.com/validar_usu.php");
             }
         });
+
+        prepararNotificaciones();
+    }
+
+    private void prepararNotificaciones(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            String channelId  = getString(R.string.default_notification_channel_id);
+            String channelName = getString(R.string.default_notification_channel_name);
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_HIGH));
+        }
+        // Get token
+        // [START retrieve_current_token]
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, msg);
+                        //Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_SHORT).show();
+                        gcmRegId = token;
+                    }
+                });
+        // [END retrieve_current_token]
     }
 
     private void validarUsuario(String URL) {
@@ -72,6 +122,7 @@ public class LoginActivity extends AppCompatActivity {
                 Map<String,String> parametros=new HashMap<String,String>();
                 parametros.put("usuario", edtUsuario.getText().toString());
                 parametros.put("password", edtPassword.getText().toString());
+                parametros.put("fcmtoken", gcmRegId);
                 return parametros;
             }
         };
